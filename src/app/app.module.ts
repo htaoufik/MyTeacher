4dcb13e1e98649bb5f49ba2aed8c0f586264890f
slebruchec <sebastien.lebruchec@netpartnering.com>
2017-10-12 10:12:31 +0200

    Registration working with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
-import {FileService} from '../services/file.service';
+import { FileService } from '../services/file.service';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    RegistrationFormComponent
   ],
   imports: [
-    BrowserModule, HttpModule
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [FileService],
   bootstrap: [AppComponent]
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { Teacher } from '../data-model/teacher';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.scss', '../app.component.scss']
+})
+export class RegistrationFormComponent {
+    registrationForm: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.createForm();
+    }
+
+    /**
+     * Creates the registration form with the validators
+     * @return void
+     */
+    createForm(): void {
+        this.registrationForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(2)] ],
+            lastName: ['', [Validators.required, Validators.minLength(2)]],
+            email: ['', [Validators.required, Validators.email] ],
+            password: ['', [Validators.required, Validators.minLength(8)] ],
+        });
+    }
+
+    /**
+     * Gets the CSS class depending on the formControl status
+     * @param {string} formControl
+     * @returns {string}
+     */
+    getAddOnClass(formControl: string): string {
+        const control = this.registrationForm.get(formControl);
+        if (control.valid && control.dirty) {
+            return 'add-on-valid';
+        }
+        if (control.invalid && control.dirty) {
+            return 'add-on-error';
+        }
+    }
+
+    /**
+     * Define if an error message should be shown depending on control and error
+     * @param {string} formControl
+     * @param {string} error
+     * @returns {boolean}
+     */
+    showError(formControl: string, error: string): boolean {
+        const control = this.registrationForm.get(formControl);
+        return control.hasError(error) && control.dirty;
+    }
+}
